8247ac820dae3eb5bb0858e83ce2c6623fb7d2f0
aaroncherian <aaron.cherian2@gmail.com>
2022-05-05 10:56:34 -0400

    plotting_origin_aligned things

--- plot_with_classes.py
+++ plot_with_classes.py
@@ -41,12 +41,12 @@ class skeleton_COM_Plot:
         this_freemocap_session_path = validation_data_path / sessionID
         this_freemocap_data_path = this_freemocap_session_path/'DataArrays'
 
-        totalCOM_data_path = this_freemocap_data_path / 'totalBodyCOM_frame_XYZ.npy'
-        segmentedCOM_data_path = this_freemocap_data_path / 'segmentedCOM_frame_joint_XYZ.npy'
+        totalCOM_data_path = this_freemocap_data_path / 'origin_aligned_totalBodyCOM_frame_XYZ.npy'
+        segmentedCOM_data_path = this_freemocap_data_path / 'origin_aligned_segmentedCOM_frame_joint_XYZ.npy'
         #mediapipe_data_path = this_freemocap_data_path/'mediaPipeSkel_3d_smoothed.npy'
         #mediapipe_data_path = this_freemocap_data_path/'rotated_mediaPipeSkel_3d_smoothed.npy'
-        mediapipe_data_path = this_freemocap_data_path/'mediapipe_origin_corrected_and_rotated.npy'
-        mediapipeSkeleton_file_name = this_freemocap_data_path/'mediapipeSkelcoordinates_frame_segment_joint_XYZ.pkl'
+        mediapipe_data_path = this_freemocap_data_path/'mediapipe_origin_aligned_skeleton_3D.npy'
+        mediapipeSkeleton_file_name = this_freemocap_data_path/'origin_aligned_mediapipeSkelcoordinates_frame_segment_joint_XYZ.pkl'
 
         #syncedVideoName = sessionID + '_Cam1_synced.mp4'
         syncedVideoName = sessionID + '_mediapipe_overlayed_video.mp4'
@@ -809,7 +809,7 @@ class skeleton_COM_Plot:
             ax.plot(yy, front_foot_avg_y[:]+distance_raised, self.mx-self.ax_range,zdir ='x', color = 'forestgreen',alpha = .5)
             ax.plot(yy, back_foot_avg_y[:]+distance_raised, self.mx-self.ax_range,zdir ='x', color = 'coral',alpha = .5)
 
-            ax.plot(yy[0:frame],-1*this_range_totalCOM_frame_XYZ[0:frame,1]+distance_radised,self.mx-self.ax_range,zdir ='x', color = 'black',alpha = .8)
+            ax.plot(yy[0:frame],-1*this_range_totalCOM_frame_XYZ[0:frame,1]+distance_raised,self.mx-self.ax_range,zdir ='x', color = 'black',alpha = .8)
             ax.scatter(yy[frame],-1*this_range_totalCOM_frame_XYZ[frame,1]+distance_raised,self.mx-self.ax_range,zdir ='x', color = 'magenta',alpha = .8,marker = '*', edgecolor = 'purple', s = 40)
 
             
@@ -954,7 +954,7 @@ class skeleton_COM_Plot:
         print('Starting Frame Animation') 
         ani = FuncAnimation(figure, self.animate, frames= num_frames_to_plot, interval=.1, repeat=False, fargs = (video_frames_to_plot,), init_func= self.animation_init)
         writervideo = animation.FFMpegWriter(fps=self.output_video_fps)
-        ani.save(self.this_freemocap_session_path/'freemocap_{}.mp4'.format(self.stance), writer=writervideo)
+        ani.save(self.this_freemocap_session_path/'origin_freemocap_{}.mp4'.format(self.stance), writer=writervideo)
         print('Animation has been saved to {}'.format(self.this_freemocap_session_path))
         f=2
 
