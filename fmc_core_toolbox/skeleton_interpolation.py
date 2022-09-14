219574b4ff4ee3fbbf0f1bba6ba92e3f60d19226
aaroncherian <aaron.cherian2@gmail.com>
2022-09-14 16:47:41 -0400

    Update skeleton_interpolation.py

--- fmc_core_toolbox/skeleton_interpolation.py
+++ fmc_core_toolbox/skeleton_interpolation.py
@@ -1,22 +1,19 @@
 
-import socket
-
-from pathlib import Path
 
 import numpy as np      
 import pandas as pd
 from rich.progress import track
 
 
-def interpolate_skeleton(skeleton_3d_data):
+def interpolate_freemocap_data(freemocap_marker_data:np.ndarray):
     """ Takes in a 3d skeleton numpy array from freemocap and interpolates missing NaN values"""
-    num_frames = skeleton_3d_data.shape[0]
-    num_markers = skeleton_3d_data.shape[1]
+    num_frames = freemocap_marker_data.shape[0]
+    num_markers = freemocap_marker_data.shape[1]
 
     skel_3d_interpolated = np.empty((num_frames, num_markers, 3))
 
-    for marker in track(range(num_markers)):
-        this_marker_skel3d_data = skeleton_3d_data[:,marker,:]
+    for marker in track(range(num_markers), description= 'Interpolating Data'):
+        this_marker_skel3d_data = freemocap_marker_data[:,marker,:]
         df = pd.DataFrame(this_marker_skel3d_data)
         df2 = df.interpolate(method = 'linear',axis = 0) #use pandas interpolation methods to fill in missing data
         this_marker_interpolated_skel3d_array = np.array(df2)
@@ -33,17 +30,11 @@ def interpolate_skeleton(skeleton_3d_data):
 
 if __name__ == '__main__':
     
-    this_computer_name = socket.gethostname()
+    from pathlib import Path
+    
 
-    if this_computer_name == 'DESKTOP-F5LCT4Q':
-        #freemocap_validation_data_path = Path(r"C:\Users\aaron\Documents\HumonLab\Spring2022\ValidationStudy\FreeMocap_Data")
-        #freemocap_data_folder_path = Path(r'D:\freemocap2022\FreeMocap_Data')
-        freemocap_data_folder_path = Path(r'D:\ValidationStudy2022\FreeMocap_Data')
-    else:
-        freemocap_data_folder_path = Path(r'C:\Users\Aaron\Documents\sessions\FreeMocap_Data')
+    freemocap_data_folder_path = Path(r'D:\ValidationStudy2022\FreeMocap_Data')
 
-    #sessionID = 'sesh_2022-05-12_15_13_02'  
-    #sessionID = 'sesh_2022-06-28_12_55_34'
 
     sessionID = 'sesh_2022-05-24_15_55_40_JSM_T1_BOS'
     data_array_folder = 'DataArrays'
@@ -53,8 +44,8 @@ if __name__ == '__main__':
 
 
 
-    skel3d_data = np.load(data_array_folder_path / array_name)
-    skel_3d_interpolated = interpolate_skeleton(skel3d_data)
+    freemocap_marker_data = np.load(data_array_folder_path / array_name)
+    freemocap_marker_data_interpolated = interpolate_freemocap_data(freemocap_marker_data)
 
 
     f= 2
