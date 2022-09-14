c6e6ea4c0ef4b1408352d995d69a5760c43dc9d6
aaroncherian <aaron.cherian2@gmail.com>
2022-09-14 17:00:03 -0400

    removing plot from alignment

--- fmc_core_toolbox/debugging_plot_tools.py
+++ fmc_core_toolbox/debugging_plot_tools.py
@@ -6,27 +6,96 @@ from pathlib import Path
 
 import scipy.io as sio
 
-def set_axes_ranges(plot_ax,skeleton_data,ax_range):
-
-    mx = np.nanmean(skeleton_data[:,0])
-    my = np.nanmean(skeleton_data[:,1])
-    mz = np.nanmean(skeleton_data[:,2])
-
-    plot_ax.set_xlim(mx-ax_range,mx+ax_range)
-    plot_ax.set_ylim(my-ax_range,my+ax_range)
-    plot_ax.set_zlim(mz-ax_range,mz+ax_range)
+from skeleton_data_holder import SkeletonDataHolder
 
 
-def plot_3D_skeleton(skeleton_data, frame_to_plot):
-    this_frame_skeleton_data = skeleton_data[frame_to_plot,:,:]
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+def plot_all_skeletons(raw_skeleton_data,origin_translated_skeleton_data,y_aligned_skeleton_data,spine_aligned_skeleton_data, skeleton_indices, good_frame):
+    def create_vector(point1,point2): 
+        """Put two points in, make a vector"""
+        vector = point2 - point1
+        return vector
 
-    ax.scatter(this_frame_skeleton_data[:,0],this_frame_skeleton_data[:,1],this_frame_skeleton_data[:,2], c='r')
+    def plot_origin_vectors(plot_ax,x_vector,y_vector,z_vector,origin):
+        Zvector_X,Zvector_Y,Zvector_Z = zip(z_vector*800)
+        Xvector_X,Xvector_Y,Xvector_Z = zip(x_vector*800)
+        Yvector_X,Yvector_Y,Yvector_Z = zip(y_vector*800)
 
-    plt.show()
+        Origin_X,Origin_Y,Origin_Z = zip(origin)
 
-    set_axes_ranges(ax,this_frame_skeleton_data,1000)
+        plot_ax.quiver(Origin_X,Origin_Y,Origin_Z,Xvector_X,Xvector_Y,Xvector_Z,arrow_length_ratio=0.1,color='r', label = 'X-axis')
+        plot_ax.quiver(Origin_X,Origin_Y,Origin_Z,Yvector_X,Yvector_Y,Yvector_Z,arrow_length_ratio=0.1,color='g', label = 'Y-axis')
+        plot_ax.quiver(Origin_X,Origin_Y,Origin_Z,Zvector_X,Zvector_Y,Zvector_Z,arrow_length_ratio=0.1,color='b', label = 'Z-axis')            
+    
+    def set_axes_ranges(plot_ax,skeleton_data, ax_range):
+
+        mx = np.nanmean(skeleton_data[:,0])
+        my = np.nanmean(skeleton_data[:,1])
+        mz = np.nanmean(skeleton_data[:,2])
+    
+        plot_ax.set_xlim(mx-ax_range,mx+ax_range)
+        plot_ax.set_ylim(my-ax_range,my+ax_range)
+        plot_ax.set_zlim(mz-ax_range,mz+ax_range)        
+
+    def plot_spine_unit_vector(plot_ax,skeleton_mid_hip_XYZ,skeleton_spine_unit_vector):
+
+        skeleton_spine_unit_x, skeleton_spine_unit_y, skeleton_spine_unit_z = zip(skeleton_spine_unit_vector*800)
+
+        plot_ax.quiver(skeleton_mid_hip_XYZ[0],skeleton_mid_hip_XYZ[1],skeleton_mid_hip_XYZ[2], skeleton_spine_unit_x,skeleton_spine_unit_y,skeleton_spine_unit_z,arrow_length_ratio=0.1,color='pink')
+
+    def plot_heel_unit_vector(plot_ax,skeleton_heel_unit_vector, heel_vector_origin_XYZ):
+
+        origin_heel_x, origin_heel_y, origin_heel_z = zip(heel_vector_origin_XYZ)
+        skeleton_heel_unit_x, skeleton_heel_unit_y, skeleton_heel_unit_z = zip(skeleton_heel_unit_vector*500)
+
+        plot_ax.quiver(origin_heel_x,origin_heel_y,origin_heel_z, skeleton_heel_unit_x,skeleton_heel_unit_y,skeleton_heel_unit_z,arrow_length_ratio=0.1,color='orange')
+
+    def plot_vectors_for_skeleton(plot_ax,skeleton_data_holder):
+        heel_unit_vector = skeleton_data_holder.heel_unit_vector
+        heel_vector_origin = skeleton_data_holder.heel_vector_origin
+        mid_hip_XYZ = skeleton_data_holder.mid_hip_XYZ
+        spine_unit_vector = skeleton_data_holder.spine_unit_vector
+
+        plot_heel_unit_vector(plot_ax,heel_unit_vector,heel_vector_origin)
+        plot_spine_unit_vector(plot_ax,mid_hip_XYZ,spine_unit_vector)
+
+    origin = np.array([0, 0, 0])
+    x_axis = np.array([1, 0, 0])
+    y_axis = np.array([0, 1, 0])
+    z_axis = np.array([0, 0, 1])
+
+    x_vector = create_vector(origin,x_axis)
+    y_vector = create_vector(origin,y_axis)
+    z_vector = create_vector(origin,z_axis)
+    figure = plt.figure()
+
+    ax1 = figure.add_subplot(221,projection = '3d')
+    ax2 = figure.add_subplot(222,projection = '3d')
+    ax3 = figure.add_subplot(223,projection = '3d')
+    ax4 = figure.add_subplot(224,projection = '3d')
+
+    axes_list = [ax1,ax2,ax3,ax4]
+
+    ax1.set_title('Original Skeleton')
+    ax2.set_title('Skeleton Translated to Origin')
+    ax3.set_title('Skeleton Rotated to Make +Y Forwards')
+    ax4.set_title('Skeleton Rotated to Make +Z Up')
+
+    raw_skeleton_holder = SkeletonDataHolder(raw_skeleton_data, skeleton_indices,good_frame)
+    origin_translated_skeleton_holder = SkeletonDataHolder(origin_translated_skeleton_data, skeleton_indices,good_frame)
+    y_aligned_skeleton_holder = SkeletonDataHolder(y_aligned_skeleton_data, skeleton_indices,good_frame)
+    spine_aligned_skeleton_holder = SkeletonDataHolder(spine_aligned_skeleton_data, skeleton_indices,good_frame)
+
+    skeleton_holder_list = [raw_skeleton_holder, origin_translated_skeleton_holder, y_aligned_skeleton_holder,spine_aligned_skeleton_holder]
+
+    ax_range = 1800
+    for ax,skeleton_data_holder in zip(axes_list,skeleton_holder_list):
+        set_axes_ranges(ax,skeleton_data_holder.good_frame_skeleton_data,ax_range)
+        plot_origin_vectors(ax,x_vector,y_vector,z_vector,origin)
+        ax.scatter(skeleton_data_holder.good_frame_skeleton_data[:,0], skeleton_data_holder.good_frame_skeleton_data[:,1], skeleton_data_holder.good_frame_skeleton_data[:,2], c = 'r')
+        plot_vectors_for_skeleton(ax,skeleton_data_holder)
+
+    ax3.legend()
+    ax4.legend()
 
     plt.show()
 
--- fmc_core_toolbox/skeleton_origin_alignment.py
+++ fmc_core_toolbox/skeleton_origin_alignment.py
@@ -1,11 +1,10 @@
 
-from pathlib import Path 
 
 import numpy as np
-import matplotlib.pyplot as plt
+
 from rich.progress import track
 
-from skeleton_data_holder import SkeletonDataHolder
+
 
 
 
@@ -128,95 +127,10 @@ def rotate_skeleton_to_vector(reference_vector, vector_to_rotate_to, original_sk
 
 
 
-def plot_all_skeletons(raw_skeleton_data,origin_translated_skeleton_data,y_aligned_skeleton_data,spine_aligned_skeleton_data, good_frame):
-
-    def plot_origin_vectors(plot_ax,x_vector,y_vector,z_vector,origin):
-        Zvector_X,Zvector_Y,Zvector_Z = zip(z_vector*800)
-        Xvector_X,Xvector_Y,Xvector_Z = zip(x_vector*800)
-        Yvector_X,Yvector_Y,Yvector_Z = zip(y_vector*800)
-
-        Origin_X,Origin_Y,Origin_Z = zip(origin)
-
-        plot_ax.quiver(Origin_X,Origin_Y,Origin_Z,Xvector_X,Xvector_Y,Xvector_Z,arrow_length_ratio=0.1,color='r', label = 'X-axis')
-        plot_ax.quiver(Origin_X,Origin_Y,Origin_Z,Yvector_X,Yvector_Y,Yvector_Z,arrow_length_ratio=0.1,color='g', label = 'Y-axis')
-        plot_ax.quiver(Origin_X,Origin_Y,Origin_Z,Zvector_X,Zvector_Y,Zvector_Z,arrow_length_ratio=0.1,color='b', label = 'Z-axis')            
-    
-    def set_axes_ranges(plot_ax,skeleton_data, ax_range):
-
-        mx = np.nanmean(skeleton_data[:,0])
-        my = np.nanmean(skeleton_data[:,1])
-        mz = np.nanmean(skeleton_data[:,2])
-    
-        plot_ax.set_xlim(mx-ax_range,mx+ax_range)
-        plot_ax.set_ylim(my-ax_range,my+ax_range)
-        plot_ax.set_zlim(mz-ax_range,mz+ax_range)        
-
-    def plot_spine_unit_vector(plot_ax,skeleton_mid_hip_XYZ,skeleton_spine_unit_vector):
-
-        skeleton_spine_unit_x, skeleton_spine_unit_y, skeleton_spine_unit_z = zip(skeleton_spine_unit_vector*800)
-
-        plot_ax.quiver(skeleton_mid_hip_XYZ[0],skeleton_mid_hip_XYZ[1],skeleton_mid_hip_XYZ[2], skeleton_spine_unit_x,skeleton_spine_unit_y,skeleton_spine_unit_z,arrow_length_ratio=0.1,color='pink')
-
-    def plot_heel_unit_vector(plot_ax,skeleton_heel_unit_vector, heel_vector_origin_XYZ):
-
-        origin_heel_x, origin_heel_y, origin_heel_z = zip(heel_vector_origin_XYZ)
-        skeleton_heel_unit_x, skeleton_heel_unit_y, skeleton_heel_unit_z = zip(skeleton_heel_unit_vector*500)
-
-        plot_ax.quiver(origin_heel_x,origin_heel_y,origin_heel_z, skeleton_heel_unit_x,skeleton_heel_unit_y,skeleton_heel_unit_z,arrow_length_ratio=0.1,color='orange')
-
-    def plot_vectors_for_skeleton(plot_ax,skeleton_data_holder):
-        heel_unit_vector = skeleton_data_holder.heel_unit_vector
-        heel_vector_origin = skeleton_data_holder.heel_vector_origin
-        mid_hip_XYZ = skeleton_data_holder.mid_hip_XYZ
-        spine_unit_vector = skeleton_data_holder.spine_unit_vector
-
-        plot_heel_unit_vector(plot_ax,heel_unit_vector,heel_vector_origin)
-        plot_spine_unit_vector(plot_ax,mid_hip_XYZ,spine_unit_vector)
-
-    origin = np.array([0, 0, 0])
-    x_axis = np.array([1, 0, 0])
-    y_axis = np.array([0, 1, 0])
-    z_axis = np.array([0, 0, 1])
-
-    x_vector = create_vector(origin,x_axis)
-    y_vector = create_vector(origin,y_axis)
-    z_vector = create_vector(origin,z_axis)
-    figure = plt.figure()
-
-    ax1 = figure.add_subplot(221,projection = '3d')
-    ax2 = figure.add_subplot(222,projection = '3d')
-    ax3 = figure.add_subplot(223,projection = '3d')
-    ax4 = figure.add_subplot(224,projection = '3d')
-
-    axes_list = [ax1,ax2,ax3,ax4]
-
-    ax1.set_title('Original Skeleton')
-    ax2.set_title('Skeleton Translated to Origin')
-    ax3.set_title('Skeleton Rotated to Make +Y Forwards')
-    ax4.set_title('Skeleton Rotated to Make +Z Up')
-
-    raw_skeleton_holder = SkeletonDataHolder(raw_skeleton_data,mediapipe_indices,good_frame)
-    origin_translated_skeleton_holder = SkeletonDataHolder(origin_translated_skeleton_data,mediapipe_indices,good_frame)
-    y_aligned_skeleton_holder = SkeletonDataHolder(y_aligned_skeleton_data,mediapipe_indices,good_frame)
-    spine_aligned_skeleton_holder = SkeletonDataHolder(spine_aligned_skeleton_data,mediapipe_indices,good_frame)
-
-    skeleton_holder_list = [raw_skeleton_holder, origin_translated_skeleton_holder, y_aligned_skeleton_holder,spine_aligned_skeleton_holder]
-
-    ax_range = 1800
-    for ax,skeleton_data_holder in zip(axes_list,skeleton_holder_list):
-        set_axes_ranges(ax,skeleton_data_holder.good_frame_skeleton_data,ax_range)
-        plot_origin_vectors(ax,x_vector,y_vector,z_vector,origin)
-        ax.scatter(skeleton_data_holder.good_frame_skeleton_data[:,0], skeleton_data_holder.good_frame_skeleton_data[:,1], skeleton_data_holder.good_frame_skeleton_data[:,2], c = 'r')
-        plot_vectors_for_skeleton(ax,skeleton_data_holder)
-
-    ax3.legend()
-    ax4.legend()
-
-    plt.show()
 
 
 
-def align_skeleton_with_origin(skeleton_data, skeleton_indices, good_frame, debug = False):
+def align_skeleton_with_origin(skeleton_data, skeleton_indices, good_frame):
 
     """
     Takes in freemocap skeleton data and translates the skeleton to the origin, and then rotates the data 
@@ -265,21 +179,19 @@ def align_skeleton_with_origin(skeleton_data, skeleton_indices, good_frame, debu
     #Rotating the skeleton so that the spine is aligned with +z
     y_aligned_mid_hip_XYZ = calculate_mid_hip_XYZ_coordinates(y_aligned_skeleton_data[good_frame,:,:],left_hip_index,right_hip_index)
     y_aligned_mid_shoulder_XYZ = calculate_shoulder_center_XYZ_coordinates(y_aligned_skeleton_data[good_frame,:,:], left_shoulder_index, right_shoulder_index)
-    
     y_aligned_spine_vector = create_vector(y_aligned_mid_hip_XYZ,y_aligned_mid_shoulder_XYZ)
     
     spine_aligned_skeleton_data = rotate_skeleton_to_vector(y_aligned_spine_vector,z_vector,y_aligned_skeleton_data)
 
-    if debug:
-        plot_all_skeletons(skeleton_data,foot_translated_skeleton_data,y_aligned_skeleton_data,spine_aligned_skeleton_data,good_frame)
-   
-    return spine_aligned_skeleton_data
+    return spine_aligned_skeleton_data, y_aligned_skeleton_data, foot_translated_skeleton_data
 
 
 
 if __name__ == '__main__':
 
     from mediapipe_skeleton_builder import mediapipe_indices
+    from pathlib import Path
+    from debugging_plot_tools import plot_all_skeletons 
 
     freemocap_data_folder_path = Path(r'D:\freemocap2022\FreeMocap_Data')
     sessionID = 'sesh_2022-05-09_12_20_23'
@@ -292,7 +204,11 @@ if __name__ == '__main__':
 
 
     good_frame = 81 #a random number
+    debug = True
 
-    origin_aligned_skeleton_data = align_skeleton_with_origin(skeleton_data,skeleton_indices, good_frame, debug = True)
+    origin_aligned_skeleton_data, y_aligned_skeleton_data,foot_translated_skeleton_data = align_skeleton_with_origin(skeleton_data,skeleton_indices, good_frame)
 
+    if debug:
+        plot_all_skeletons(skeleton_data,foot_translated_skeleton_data,y_aligned_skeleton_data,origin_aligned_skeleton_data,mediapipe_indices,good_frame)
+   
     f = 2
