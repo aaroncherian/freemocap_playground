c481799d07c35fe5d1d116494ecd1315cb4c24ff
aaroncherian <aaron.cherian2@gmail.com>
2022-09-14 16:50:06 -0400

    Update skeleton_filtering.py

--- fmc_core_toolbox/skeleton_filtering.py
+++ fmc_core_toolbox/skeleton_filtering.py
@@ -1,12 +1,10 @@
 
 
-import socket
-from pathlib import Path
-
 from scipy import signal
 import numpy as np
+from rich.progress import track
 
-def butter_lowpass_filter(data, cutoff, sampling_rate, order):
+def butter_lowpass_filter(data:np.ndarray, cutoff:float, sampling_rate:int, order:int):
     """ Run a low pass butterworth filter on a single column of data"""
     nyquist_freq = 0.5*sampling_rate
     normal_cutoff = cutoff / nyquist_freq
@@ -22,7 +20,7 @@ def filter_freemocap_data(freemocap_marker_data:np.ndarray, cutoff:float, sampli
     num_markers = freemocap_marker_data.shape[1]
     filtered_data = np.empty((num_frames,num_markers,3))
 
-    for marker in range(num_markers):
+    for marker in track(range(num_markers), description = 'Filtering Data'):
         for x in range(3):
             filtered_data[:,marker,x] = butter_lowpass_filter(freemocap_marker_data[:,marker,x],cutoff,sampling_rate,order)
 
@@ -30,15 +28,12 @@ def filter_freemocap_data(freemocap_marker_data:np.ndarray, cutoff:float, sampli
     return filtered_data
 
 if __name__ == '__main__':
+        
 
-    this_computer_name = socket.gethostname()
+    from pathlib import Path
 
-    if this_computer_name == 'DESKTOP-F5LCT4Q':
-        #freemocap_validation_data_path = Path(r"C:\Users\aaron\Documents\HumonLab\Spring2022\ValidationStudy\FreeMocap_Data")
-        #freemocap_data_folder_path = Path(r'D:\freemocap2022\FreeMocap_Data')
-        freemocap_data_folder_path = Path(r'D:\ValidationStudy2022\FreeMocap_Data')
-    else:
-        freemocap_data_folder_path = Path(r'C:\Users\Aaron\Documents\sessions\FreeMocap_Data')
+    freemocap_data_folder_path = Path(r'D:\ValidationStudy2022\FreeMocap_Data')
+
         
     sessionID = 'sesh_2022-05-24_15_55_40_JSM_T1_BOS'
     data_array_folder = 'DataArrays'
