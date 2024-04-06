85185afeb0a4911dce6ed4d2cd7d2f1d579282f0
aaroncherian <aaron.cherian2@gmail.com>
2024-04-05 23:45:33 -0400

    example usage

--- unified_skeleton/unified_skeleton_main.py
+++ unified_skeleton/unified_skeleton_main.py
@@ -34,9 +34,27 @@ skeleton = create_skeleton_model(
     anthropometric_data = center_of_mass_anthropometric_data
 )
 
+## Some examples of how to access different things from the skeleton
+print(f'Marker Names: {skeleton.markers.original_marker_names}') #actual marker names
+print(f'Virtual Markers: {skeleton.markers.virtual_markers.virtual_markers}') #virtual markers + weights
+print(f'All Markers: {skeleton.markers.all_markers}') #list of all markers (actual + virtual, if virtual is included)
+
+print(f'Segment Connections: {skeleton.segments.segment_connections}') #segment connections
+print(f"Proximal marker for left upper arm: {skeleton.segments.segment_connections['left_upper_arm'].proximal}") #proximal and distal markers for a segment
+
+print(f'Joint Hierarchy: {skeleton.joint_hierarchy}') #joint hierarchy
+print(f'Anthropometric Data: {skeleton.anthropometric_data}') #anthropometric data
+
 
 freemocap_data = np.load(path_to_data)
 skeleton_3d = integrate_freemocap_data_into_skeleton_model(skeleton, freemocap_data)
+
+#Accessing 3d data from the skeleton
+#for a marker
+print(f"3D Data for left shoulder: {skeleton_3d.marker_data['left_shoulder']}") #3d data for a marker (actual or virtual)
+#for a segment
+print(f"3D Data for left upper arm: {skeleton_3d.get_segment_markers('left_upper_arm')}") #3d data for proximal and distal markers of a segment
+
 
 total_body_center_of_mass = calculate_center_of_mass_from_skeleton(skeleton=skeleton_3d)
 rigid_bone_data = enforce_rigid_bones_from_skeleton(skeleton=skeleton_3d)
