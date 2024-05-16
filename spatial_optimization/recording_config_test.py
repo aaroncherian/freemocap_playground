750396c84ebd4f5592a5239b68f1af73292b230b
aaroncherian <aaron.cherian2@gmail.com>
2024-05-16 16:49:57 -0400

    Update recording_config_test.py

--- spatial_optimization/recording_config_test.py
+++ spatial_optimization/recording_config_test.py
@@ -2,8 +2,7 @@ from skellyalign.models.recording_config import RecordingConfig
 from mdn_validation_marker_set import markers_to_extract, qualisys_nih_markers, mediapipe_markers
 from pathlib import Path
 import numpy as np
-from skellymodels.create_skeleton import create_skeleton_model
-from skellymodels.model_info.mediapipe_model_info import MediapipeModelInfo
+from skellymodels.create_model_skeleton import create_mediapipe_skeleton_model, create_qualisys_skeleton_model, create_qualisys_mdn_nih_skeleton_model
 
 
 path_to_recording = Path(r"D:\2023-05-17_MDN_NIH_data\1.0_recordings\calib_3\sesh_2023-05-17_14_53_48_MDN_NIH_Trial3")
@@ -22,19 +21,19 @@ sample_recording_config = RecordingConfig(
 
 )
 
+
 freemocap_data = np.load(sample_recording_config.path_to_freemocap_output_data)
-mediapipe_model_info = MediapipeModelInfo()
-
-freemocap_model = create_skeleton_model(actual_markers=mediapipe_model_info.landmark_names, 
-                      num_tracked_points=mediapipe_model_info.num_tracked_points,
-                      segment_connections=mediapipe_model_info.segment_connections,
-                     virtual_markers=mediapipe_model_info.virtual_markers_definitions,
-                     joint_hierarchy= mediapipe_model_info.joint_hierarchy,
-                     center_of_mass_info=mediapipe_model_info.center_of_mass_definitions
-)
-
-
+freemocap_model = create_mediapipe_skeleton_model()
 freemocap_model.integrate_freemocap_3d_data(freemocap_data)
+
+
+qualisys_data = np.load(sample_recording_config.path_to_qualisys_output_data)
+qualisys_model = create_qualisys_mdn_nih_skeleton_model()
+qualisys_model.integrate_freemocap_3d_data(qualisys_data)
+
+freemocap_model.virtual_marker_names
+
+print(freemocap_model.marker_data_as_numpy.shape)
 # from skellyalign.run_alignment import run_ransac_alignment
 
 # aligned_freemocap_data = run_ransac_alignment(sample_recording_config)
