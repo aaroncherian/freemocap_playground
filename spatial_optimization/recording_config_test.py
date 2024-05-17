052acaf2e9bfb248e8638b3b2497436cbaaad3ec
aaroncherian <aaron.cherian2@gmail.com>
2024-05-17 13:25:38 -0400

    Update recording_config_test.py

--- spatial_optimization/recording_config_test.py
+++ spatial_optimization/recording_config_test.py
@@ -1,6 +1,6 @@
-from skellyalign.models.recording_config import RecordingConfig
+from skellyalign.models.alignment_config import SpatialAlignmentConfig
 from skellyalign.utilities.data_handlers import DataProcessor
-from skellyalign.align_things import get_best_transformation_matrix_ransac, apply_transformation
+from skellyalign.run_alignment import run_ransac_spatial_alignment
 from skellyalign.plots.scatter_3d import plot_3d_scatter
 from mdn_validation_marker_set import markers_to_extract, qualisys_nih_markers, mediapipe_markers
 from pathlib import Path
@@ -8,45 +8,41 @@ import numpy as np
 from skellymodels.create_model_skeleton import create_mediapipe_skeleton_model, create_qualisys_skeleton_model, create_qualisys_mdn_nih_skeleton_model
 
 
-path_to_recording = Path(r"D:\2023-05-17_MDN_NIH_data\1.0_recordings\calib_3\sesh_2023-05-17_14_53_48_MDN_NIH_Trial3")
+path_to_recording = Path(r"D:\2024-04-25_P01\1.0_recordings\sesh_2024-04-25_14_45_59_P01_NIH_Trial1")
+path_to_freemocap_output_data = path_to_recording/'output_data'/'mediapipe_body_3d_xyz.npy'
+path_to_qualisys_output_data = path_to_recording/'qualisys_data'/ 'qualisys_joint_centers_3d_xyz.npy'
 
 
-sample_recording_config = RecordingConfig(
-    path_to_recording=path_to_recording,
-    path_to_freemocap_output_data = path_to_recording/'output_data'/'mediapipe_body_3d_xyz.npy',
-    path_to_qualisys_output_data = path_to_recording/'qualisys'/ 'clipped_qualisys_skel_3d.npy',
-    freemocap_markers=mediapipe_markers,
-    qualisys_markers=qualisys_nih_markers,
-    markers_for_alignment=markers_to_extract,
-    frames_to_sample=20,
-    max_iterations=20,
-    inlier_threshold=50
-
-)
-
-
-freemocap_data = np.load(sample_recording_config.path_to_freemocap_output_data)
+freemocap_data = np.load(path_to_freemocap_output_data)
 freemocap_model = create_mediapipe_skeleton_model()
 freemocap_model.integrate_freemocap_3d_data(freemocap_data)
 
 
-qualisys_data = np.load(sample_recording_config.path_to_qualisys_output_data)
-qualisys_model = create_qualisys_mdn_nih_skeleton_model()
+qualisys_data = np.load(path_to_qualisys_output_data)
+qualisys_model = create_qualisys_skeleton_model()
 qualisys_model.integrate_freemocap_3d_data(qualisys_data)
 
-freemocap_model.virtual_marker_names
+alignment_config = SpatialAlignmentConfig(
+    freemocap_skeleton=freemocap_model,
+    qualisys_skeleton=qualisys_model,
+    markers_for_alignment=markers_to_extract,
+    frames_to_sample=20,
+    max_iterations=50,
+    inlier_threshold=40
+)
 
-print(freemocap_model.marker_data_as_numpy.shape)
+aligned_freemocap_data, transformation_matrix = run_ransac_spatial_alignment(alignment_config)
 
-freemocap_data_processor = DataProcessor(data=freemocap_model.marker_data_as_numpy, marker_list=freemocap_model.marker_names, markers_for_alignment=markers_to_extract)
-qualisys_data_processor = DataProcessor(data=qualisys_model.marker_data_as_numpy, marker_list=qualisys_model.marker_names, markers_for_alignment=markers_to_extract)
 
-best_transformation_matrix = get_best_transformation_matrix_ransac(freemocap_data=freemocap_data_processor.extracted_data_3d, qualisys_data=qualisys_data_processor.extracted_data_3d, frames_to_sample = 20, max_iterations=50, inlier_threshold= 40, )
+# freemocap_data_processor = DataProcessor(data=freemocap_model.marker_data_as_numpy, marker_list=freemocap_model.marker_names, markers_for_alignment=markers_to_extract)
+# qualisys_data_processor = DataProcessor(data=qualisys_model.marker_data_as_numpy, marker_list=qualisys_model.marker_names, markers_for_alignment=markers_to_extract)
 
-freemocap_aligned_data = apply_transformation(best_transformation_matrix, freemocap_model.marker_data_as_numpy)
+# best_transformation_matrix = get_best_transformation_matrix_ransac(freemocap_data=freemocap_data_processor.extracted_data_3d, qualisys_data=qualisys_data_processor.extracted_data_3d, frames_to_sample = 20, max_iterations=50, inlier_threshold= 40, )
 
-plot_3d_scatter(freemocap_data=freemocap_aligned_data, qualisys_data=qualisys_model.marker_data_as_numpy)
-# from skellyalign.run_alignment import run_ransac_alignment
+# freemocap_aligned_data = apply_transformation(best_transformation_matrix, freemocap_model.marker_data_as_numpy)
+
+plot_3d_scatter(freemocap_data=aligned_freemocap_data, qualisys_data=qualisys_model.marker_data_as_numpy)
+# # from skellyalign.run_alignment import run_ransac_alignment
 
 # aligned_freemocap_data = run_ransac_alignment(sample_recording_config)
 
