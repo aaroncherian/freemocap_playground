8e0ac5007ea927e91b7c951b6aac6b055d4bb01a
aaroncherian <aaron.cherian2@gmail.com>
2024-08-09 17:01:02 -0400

    adding slider

---
+++ skeleton-visualization/vue_fastapi/frontend/src/components/Slider.vue
@@ -0,0 +1,42 @@
+<template>
+  <div ref="controlContainer" class="control-container">
+    <input
+        ref="frameSlider"
+        type="range"
+        min="0"
+        v-model="currentFrameNumber"
+        :max="maxFrames"
+        class="slider"
+    />
+    <span class="frame-label"> Frame: {{ currentFrameNumber }} </span>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted} from 'vue';
+const frameSlider = ref(null);
+const currentFrameNumber = ref(null);
+const maxFrames = ref(0);  // Initial max value
+
+onMounted(() => {
+});
+</script>
+
+<style scoped>
+.control-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0;
+  background-color: #fefefe;
+}
+.slider {
+  width: 70%;
+  margin: 0 auto;
+}
+
+.frame-label {
+  color: black;
+}
+</style>
