efc88c0b6e8a3035559fa873433413b9e0274a75
aaroncherian <aaron.cherian2@gmail.com>
2024-12-09 15:45:09 -0500

    Update resample_qualisys_data.py

--- qualisys/refactored_processing_pipelines/resample_qualisys_data.py
+++ qualisys/refactored_processing_pipelines/resample_qualisys_data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd 
 from datetime import datetime
+import numpy as np
 
 recording_folder_path = Path(r"D:\2024-10-30_treadmill_pilot\processed_data\sesh_2024-10-30_15_45_14_mdn_gait_7_exposure")
 
@@ -56,11 +57,35 @@ def create_and_insert_unix_timestamp_column(df, start_timestamp, lag_in_seconds=
     
     return df
 
+def create_freemocap_unix_timestamps(csv_path):
+    df = pd.read_csv(csv_path)
+    df.replace(-1, float('nan'), inplace=True)
+    mean_timestamps = df.iloc[:, 2:].mean(axis=1, skipna=True)
+    time_diff = np.diff(mean_timestamps)
+    framerate = 1 / np.nanmean(time_diff)
+    print(f"Calculated FreeMoCap framerate: {framerate}")
+    return mean_timestamps, framerate
+
+def synchronize_qualisys_data(qualisys_df, freemocap_timestamps):
+    synchronized_rows = {}
+    for frame_number, timestamp in enumerate(freemocap_timestamps):
+        if frame_number + 1 < len(freemocap_timestamps):
+            next_timestamp = freemocap_timestamps[frame_number + 1]
+            rows_in_range = qualisys_df.loc[(qualisys_df['unix_timestamps'] >= timestamp) & (qualisys_df['unix_timestamps'] < next_timestamp)]
+            mean_row = rows_in_range.mean(axis=0, skipna=True)
+        else:
+            rows_in_range = qualisys_df.loc[(qualisys_df['unix_timestamps'] >= timestamp)]
+            mean_row = rows_in_range.iloc[0]
+        synchronized_rows[frame_number] = mean_row
+    return pd.DataFrame.from_dict(synchronized_rows, orient='index', columns=qualisys_df.columns)
+
+
 header_length = get_header_length(qualisys_marker_tsv_path)
 qualisys_markers = pd.read_csv(qualisys_marker_tsv_path, delimiter='\t', skiprows=header_length)
-
 qualisys_unix_start_time = get_starting_qualisys_timestamp(qualisys_marker_tsv_path)
-
 qualisys_markers_with_unix = create_and_insert_unix_timestamp_column(qualisys_markers, qualisys_unix_start_time)
 
+freemocap_timestamps, framerate = create_freemocap_unix_timestamps(freemocap_csv_path)
+
+synchronized_qualisys_markers = synchronize_qualisys_data(qualisys_markers_with_unix, freemocap_timestamps)
 f = 2
